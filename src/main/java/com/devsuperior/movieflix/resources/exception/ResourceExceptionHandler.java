419c96b0bd9c86995e63ed614275b9c2adb0b8d4
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-06-29 20:06:56 -0300

    Exceptions classes

--- src/main/java/com/devsuperior/movieflix/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/movieflix/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.devsuperior.movieflix.resources.exception;
 
+import java.time.Instant;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -7,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
 
 @ControllerAdvice
@@ -16,6 +19,19 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<OAuthCustomError> unathourized(UnauthorizedException e, HttpServletRequest request) {
 		OAuthCustomError err = new OAuthCustomError("Unathorized", e.getMessage());
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(err);
+	}
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
 	}
 
 }
