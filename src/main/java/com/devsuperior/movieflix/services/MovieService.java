4562a60145b5c48b91dbfbf591842df3682afbec
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-06-30 22:51:48 -0300

    Fix: Movie Service

--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.movieflix.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,13 +10,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.dto.MovieByGenreDTO;
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.projections.MovieByGenreProjection;
 import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.repositories.UserRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -25,7 +32,13 @@ public class MovieService {
 	
 	@Autowired
 	private GenreRepository genreRepository;
-	
+
+	@Autowired
+	private ReviewRepository reviewRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+		
 	@Transactional(readOnly = true)
 	public MovieDTO findById(Long id) {
 		Optional<Movie> obj = repository.findById(id);
@@ -40,4 +53,15 @@ public class MovieService {
 		return page.map(x -> new MovieByGenreDTO(x));
 	}
 
+	@Transactional(readOnly = true)
+	public List<ReviewDTO> findAllMovieReviews(Long id){	
+
+		Movie movie = (id == 0) ? null : repository.getOne(id);
+
+		List<Review> list = reviewRepository.findAllMovieReviews(movie);
+		
+		return list.stream().map(x -> new ReviewDTO(x,x.getUser())).collect(Collectors.toList());
+	}
+
+	
 }
