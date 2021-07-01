16d9c77be16a6999d17b9465d91fcea193c7923b
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-06-30 22:39:25 -0300

    Fix: ResourceServerConfig

--- src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -21,7 +22,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"};
+	private static final String[] VISITOR_OR_MEMBER = {"/genres/**","/movies/**"};
+	private static final String[] MEMBER = {"/reviews/**"};
 	
 
 	@Override
@@ -38,7 +41,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.anyRequest().authenticated();
+		.antMatchers(HttpMethod.GET, VISITOR_OR_MEMBER).hasAnyRole("MEMBER","VISITOR")
+		.antMatchers(HttpMethod.POST, MEMBER).hasRole("MEMBER")
+		.anyRequest().authenticated(); 
 	}
 
 	
